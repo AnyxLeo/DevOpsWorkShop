31858b9f500a2c1e33a5da1857aa100e0f75ad54
AnyxLeo <anyx.leo@gmail.com>
2020-06-11 19:35:31 -0500

    isAna

--- index.test.js
+++ index.test.js
@@ -2,10 +2,10 @@ var isAna = require('./index');
 
 describe('Test is Ana', function(){
     it('should return true passing Ana', function(){
-        expect(isGeo('Ana')).toBeTruthy();
+        expect(isAna('Ana')).toBeTruthy();
     });
  
     it('should return false passing Google', function(){
-        expect(isGeo('Goolge')).toBeFalsy();
+        expect(isAna('Goolge')).toBeFalsy();
     });
 });
