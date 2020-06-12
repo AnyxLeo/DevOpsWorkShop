ea6a331b558dba546ad18b0ad0ffc344da30cd8f
AnyxLeo <anyx.leo@gmail.com>
2020-06-11 19:41:10 -0500

    WIP

--- index.test.js
+++ index.test.js
@@ -1,11 +1,11 @@
-var isAna = require('./index');
+var isAna = require('./index')
 
-describe('Test is Ana', function(){
-    it('should return true passing Ana', function(){
-        expect(isAna('Ana')).toBeTruthy();
-    });
- 
-    it('should return false passing Google', function(){
-        expect(isAna('Goolge')).toBeFalsy();
-    });
-});
+describe('Test is Ana', function () {
+  it('should return true passing Ana', function () {
+    expect(isAna('Ana')).toBeTruthy()
+  })
+
+  it('should return false passing Google', function () {
+    expect(isAna('Goolge')).toBeFalsy()
+  })
+})
