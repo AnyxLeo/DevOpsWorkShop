ef5b64bbe9025c9ee932ac22b9378255208a7102
AnyxLeo <anyx.leo@gmail.com>
2020-06-11 19:16:49 -0500

    wip

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ var isAna = require('./index');
 
 describe('Test is Ana', function(){
     it('should return true passing Ana', function(){
-        expect(isGeo('Geo')).toBeTruthy();
+        expect(isGeo('Ana')).toBeTruthy();
     });
  
     it('should return false passing Google', function(){
